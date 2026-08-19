25fe449440dc9e291e0446160f9ab8dbb77855cf
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-08-18 22:20:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,25 @@
-// Se ejecuta cuando el service worker se instala
 self.addEventListener('install', event => {
   console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './icon.png'
+      ]);
+    })
+  );
 });
 
-// Se ejecuta en cada petición de la página
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return new Response("Recurso no disponible offline");
+      });
+    })
+  );
 });
