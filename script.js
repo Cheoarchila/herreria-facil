eed58f95430e816b1ca96665a450903db8eaa63c
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-08-18 19:11:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function saludar() {
   alert("¡Hola José! Tu app está viva 🚀");
 }
+
+// Aquí registramos el service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(() => console.log('Service Worker registrado'))
+    .catch(error => console.log('Error al registrar el SW:', error));
+}
