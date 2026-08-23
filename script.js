d55dca2186c6b2d51a02a76e5a5bcd83bb14d81e
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-08-23 11:36:10 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function saludar() {
-  alert("¡Hola José! Tu app está conectada al Script.JS 🚀");
-}
-// Aquí registramos el service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(() => console.log('Service Worker registrado'))
-    .catch(error => console.log('Error al registrar el SW:', error));
-}
